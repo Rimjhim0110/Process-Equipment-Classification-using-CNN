cd897dfc580f5939565cb58d5de52ac821cbc156
Rimjhim Agarwal <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-19 12:35:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from train import train_model
 from eval import visualize, evaluate_model
   
 if __name__ == "__main__":
-    data_file = '/content/drive/MyDrive/Symbols_pixel.csv'
+    data_file = '/content/drive/MyDrive/Symbols_pixel.csv' # Add path to the CSV data file in your system
 
     # Train the model
-    model = train_model(data_file)
+    model, results, X_test, y_test = train_model(data_file)
 
     # Visualize and evaluate the model
     visualize(results)  

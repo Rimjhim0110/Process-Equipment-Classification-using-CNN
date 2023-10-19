ef6dcdfaead480e9c50e15ad1e2b64b585df7cab
Rimjhim Agarwal <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-19 12:37:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 
 def CNNmodel(num_classes):
     """
